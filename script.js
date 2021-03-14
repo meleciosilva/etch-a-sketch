7cfae898ac5c87f2e46bd561c156c339c094e027
meleciosilva <mpsilva94@gmail.com>
2021-03-13 21:09:10 -0500

    added eraser and random color button functions

--- script.js
+++ script.js
@@ -33,11 +33,33 @@ function colorGrid() {
   }
 }
 
+const eraser = document.querySelector('#eraser');
+const darken = document.querySelector('#darken');
+const pickColor = document.querySelector('#pick-color');
+const random = document.querySelector('#random');
+
+function handleEraser() {
+  eraser.addEventListener(("click"), () => {
+    const allGridBoxes = document.querySelectorAll(".grid");
+    for (let box of allGridBoxes) {
+      box.addEventListener(("mouseover"), () => {
+        box.style.backgroundColor = "#fff";
+      })
+    }
+  })
+}
+
+function handleRandom() {
+  random.addEventListener(("click"), colorGrid);
+}
+
 
 function main() {
   createGrid(16);
   resizeGrid();
   colorGrid();
+  handleEraser();
+  handleRandom();
 }
 
 window.addEventListener('DOMContentLoaded', main);
